50edfbc8c8d8ba43c2c853591fe5ecc3c974a035
hudson1998x <jhudsonjejames@gmail.com>
2026-03-10 20:51:12 +0000

    Moved If/ Else if/ Else into a safe partial now.

--- Engine/Visitors/Compiler/Compiler.cs
+++ Engine/Visitors/Compiler/Compiler.cs
@@ -53,50 +53,6 @@ public partial class Compiler : IVisitor
         throw new NotImplementedException();
     }
 
-    public void VisitIfNode(IfNode node, in ReadOnlySpan<char> source)
-    {
-        var exitLabel = DefineLabel();
-        var falseLabel = DefineLabel();
-
-        if (node.Expression != null)
-        {
-            node.Expression.Accept(this, source);
-            if (node.Expression is VariableNode)
-                Emit(OpCodes.Unbox_Any, typeof(int));
-            Emit(OpCodes.Brfalse, falseLabel);
-        }
-
-        if (node.Body != null)
-            node.Body.Accept(this, source);
-
-        bool bodyExits = false;
-        if (node.Body != null && node.Body.Statements.Count > 0)
-        {
-            for (int i = 0; i < node.Body.Statements.Count; i++)
-            {
-                if (node.Body.Statements[i] is BreakNode breakNode)
-                {
-                    breakNode.Accept(this, source);
-                    bodyExits = true;
-                    break;
-                }
-            }
-        }
-
-        if (!bodyExits)
-            Emit(OpCodes.Br, exitLabel);
-
-        MarkLabel(falseLabel);
-
-        foreach (var elseIf in node.ElseIfs)
-            elseIf.Accept(this, source);
-
-        if (node.ElseNode != null)
-            node.ElseNode.Accept(this, source);
-
-        MarkLabel(exitLabel);
-    }
-
     public void VisitTernaryNode(TernaryNode node, in ReadOnlySpan<char> source)
     {
         throw new NotImplementedException();
---
+++ Engine/Visitors/Compiler/Visitor/Control/IfElseIfElseCondition.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection.Emit;
+using PHPIL.Engine.SyntaxTree;
+
+namespace PHPIL.Engine.Visitors;
+
+public partial class Compiler
+{
+    public void VisitIfNode(IfNode node, in ReadOnlySpan<char> source)
+    {
+        var exitLabel = DefineLabel();
+        var falseLabel = DefineLabel();
+
+        if (node.Expression != null)
+        {
+            node.Expression.Accept(this, source);
+            if (node.Expression is VariableNode)
+                Emit(OpCodes.Unbox_Any, typeof(int));
+            Emit(OpCodes.Brfalse, falseLabel);
+        }
+
+        if (node.Body != null)
+            node.Body.Accept(this, source);
+
+        bool bodyExits = false;
+        if (node.Body != null && node.Body.Statements.Count > 0)
+        {
+            for (int i = 0; i < node.Body.Statements.Count; i++)
+            {
+                if (node.Body.Statements[i] is BreakNode breakNode)
+                {
+                    breakNode.Accept(this, source);
+                    bodyExits = true;
+                    break;
+                }
+            }
+        }
+
+        if (!bodyExits)
+            Emit(OpCodes.Br, exitLabel);
+
+        MarkLabel(falseLabel);
+
+        foreach (var elseIf in node.ElseIfs)
+            elseIf.Accept(this, source);
+
+        if (node.ElseNode != null)
+            node.ElseNode.Accept(this, source);
+
+        MarkLabel(exitLabel);
+    }
+}
