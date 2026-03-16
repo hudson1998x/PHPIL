84bcdf08861965578a0d431dee8ff361c0ab620c
hudson1998x <jhudsonjejames@gmail.com>
2026-03-16 18:50:48 +0000

    A small issue with the array append meant appends weren't working as expected, this fixes that issue.

--- Engine/Runtime/Sdk/ArrayHelpers.cs
+++ Engine/Runtime/Sdk/ArrayHelpers.cs
@@ -7,15 +7,7 @@ public static class ArrayHelpers
 {
     public static void Append(Dictionary<object, object> dict, object value)
     {
-        int nextKey = 0;
-        
-        var intKeys = dict.Keys.OfType<int>().ToList();
-        if (intKeys.Any())
-        {
-            nextKey = intKeys.Max() + 1;
-        }
-
-        dict[nextKey] = value;
+        dict[dict.Count] = value;
     }
 
     public static Dictionary<object, object> Merge(Dictionary<object, object> target, Dictionary<object, object> source)
@@ -41,5 +33,4 @@ public static class ArrayHelpers
         }
         return target;
     }
-
 }
