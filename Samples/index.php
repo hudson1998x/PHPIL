a83f589f9c450930d9081acf7e94e03567de5ea2
hudson1998x <jhudsonjejames@gmail.com>
2026-03-16 16:20:08 +0000

    Added var_dump to superglobals on the index.php so you can see the server populate the superglobals.

--- Samples/index.php
+++ Samples/index.php
@@ -6,4 +6,7 @@ Eventing\Events::On('request', function($url) {
     print("[URL] {$url}");
 });
 
-Eventing\Events::Dispatch('request', '/path/to/users');
+Eventing\Events::Dispatch('request', $_SERVER['REQUEST_URI']);
+
+print('<pre>');
+var_dump($_SERVER);
